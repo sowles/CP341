515711157bb8009f2c2cd9786de728624dfdd398
George Sowles <MintyMilk1@gmail.com>
2024-09-25 15:44:41 -0600

    update

--- sendrecieve.c
+++ sendrecieve.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <pigpiod_if2.h>
 #include <unistd.h>
+#include <time.h>
 
 int main(){
 	//KNOWN PINS
@@ -12,26 +13,19 @@ int main(){
 	//Port 3 rx: 22
 	//Port 4 tx: 21
 	//Port 4 rx: 20
-	int pi = pigpio_start(NULL,NULL);
-	//for (int i = 0; i < 54; i++){
-	//	toggle = gpio_write(pi,27,0);
-	//	int state1 = gpio_read(pi, i);
+	int clockrate = 1000;
 
-	//	toggle = gpio_write(pi,27,1);
-	//	int state2 = gpio_read(pi, i);
-	//	printf("PIN %d: %d - %d\n", i, state1, state2);
-	//	toggle = gpio_write(pi,27,0);
-	//}
+	int pi = pigpio_start(NULL,NULL);
 	
+	/**	
 	int *sending;
 	int *recieved;
 	sending = (int*) malloc(sizeof(int)*4);
 	sending[0] = 1;	
-	sending[1] = 1;
+	sending[1] = 0;
 	sending[2] = 0;
 	sending[3] = 1;
-
-		
+	
 	recieved = (int*) malloc(sizeof(int)*4);
 	recieved[0] = 0;	
 	recieved[1] = 0;	
@@ -42,26 +36,68 @@ int main(){
 		recieved[i] = read;
 		printf("%d", recieved[i]);
 	}
-	
-	
+	*/
 
-		
-	//int pi = pigpio_start(NULL,NULL);
-		
-	//int setmode = set_mode(pi,24,PI_OUTPUT);
-	//int firsttest = gpio_read(pi,24);
-	//int toggle = gpio_write(pi,24,1);
-	//int secondtest = gpio_read(pi,24);
-	//printf("%d\n",firsttest);
-	//printf("%d\n",secondtest);
-	pigpio_stop(pi);
+	recieveHandshake(pi);
 
-	free(sending);
-	free(recieved);
+	pigpio_stop(pi);		
+	//free(sending);
+	//free(recieved);
 	return 0;
+}
+/**
+int sendHandshake(int pi){
+	int primaryState = gpio_read(pi, 20);	
+	int currentState = primaryState;
+
+	printf("primary: %d\n", primaryState);
+
+	//Wait for the reception
+	clock_t starttime = clock();
+	while(currentState == primaryState){
+		int toggle = gpio_write(pi,27,0);
+		sleep(1);
+		toggle = gpio_write(pi,27,1);
+		currentState = gpio_read(pi,20);
+		clock_t endtime = clock();
+		if((endtime-starttime)/CLOCKS_PER_SEC > 5){
+			printf("Handshake failed");
+			return 1;
+		}
+	}
+	
+	int test = gpio_read(pi, 20);
+	printf("End: %d\n", test);
+	printf("Handshake successfully sent");
 
 }
+*/
 
+int recieveHandshake(int pi){
+	int primaryState = gpio_read(pi, 20);
+	int currentState = primaryState;
+	printf("primary: %d\n", primaryState);
+
+	clock_t starttime = clock();
+	while(currentState == primaryState){
+		currentState = gpio_read(pi,20);
+		int toggle = gpio_write(pi,27,0);
+		toggle = gpio_write(pi,27,1);
+		clock_t endtime = clock();
+		if((endtime-starttime)/CLOCKS_PER_SEC > 10){
+			return 1;
+		
+		}
+	}
+
+	int toggle = gpio_write(pi,27,0);
+	toggle = gpio_write(pi,27,1);
+	
+	
+	printf("Handshake recieved");
+
+	
+}
 int packageReader(int pi, int sent){
 	int toggle = gpio_write(pi, 27, 0);
 	toggle = gpio_write(pi,27,sent);
