ccf366f068fd9577664c4c45949ee84bd1527e8a
George Sowles <MintyMilk1@gmail.com>
2024-10-07 11:41:30 -0600

    Link Layer Done

---
+++ checksum.h
@@ -0,0 +1,305 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void sender_calculate_parity(char arr[], int totalCount);
+void receiver_calculate_parity(char arr[], char recarr[], int totalCount);
+void print_array(char arr[], int size);
+void ec_encode(int * sendArr, int * retArr);
+void ec_decode(int * decArr, int * decRetArr);
+void bitFlip(int * arr, int i);
+char binaryToASCII(char *binary);
+void tester();
+/**
+int main() {
+    int tcount = 0;
+    int *sendArr = malloc(sizeof(int)*8); 
+    //first bit ignored to set
+    
+    sendArr[0] = 0;
+    sendArr[1] = 1;
+    sendArr[2] = 1;
+    sendArr[3] = 0;
+    sendArr[4] = 0;
+    sendArr[5] = 0;
+    sendArr[6] = 1;
+    sendArr[7] = 0;
+
+    int *retArr = malloc(sizeof(int)*16); 
+    //first bit ignored to set
+    retArr[0] = 0;
+    retArr[1] = 0;
+    retArr[2] = 0;
+    retArr[3] = 0;
+    retArr[4] = 0;
+    retArr[5] = 0;
+    retArr[6] = 0;
+    retArr[7] = 0;
+    retArr[8] = 0;
+    retArr[9] = 0;
+    retArr[10] = 0;
+    retArr[11] = 0;
+    retArr[12] = 0;
+    retArr[13] = 0;
+    retArr[14] = 0;
+    retArr[15] = 0;
+
+    int *decArr = malloc(sizeof(int)*16); 
+    //0110110000101011
+    
+    decArr[0] = 0;
+    decArr[1] = 1;
+    decArr[2] = 1;
+    decArr[3] = 0;
+    decArr[4] = 1;
+    decArr[5] = 1;
+    decArr[6] = 0;
+    decArr[7] = 0;
+    decArr[8] = 0;
+    decArr[9] = 0;
+    decArr[10] = 1;
+    decArr[11] = 0;
+    decArr[12] = 1;
+    decArr[13] = 0;
+    decArr[14] = 1;
+    decArr[15] = 1;
+
+    int *deRetArr = malloc(sizeof(int)*8); 
+    //first bit ignored to set
+    
+    deRetArr[0] = 0;
+    deRetArr[1] = 0;
+    deRetArr[2] = 0;
+    deRetArr[3] = 0;
+    deRetArr[4] = 0;
+    deRetArr[5] = 0;
+    deRetArr[6] = 0;
+    deRetArr[7] = 0;
+
+   //ec_encode(sendArr, retArr);
+   ec_decode(decArr, deRetArr);
+    // for (int i = 0; i < 16; i++)
+    // {
+    //     printf("%d", deRetArr[i]);
+    
+    // }   
+    // printf("\n");
+    //tester();
+    free(sendArr);
+    free(deRetArr);
+    free(retArr);
+    free(decArr);
+    return 0;
+}
+*/
+void sender_calculate_parity(char arr[], int totalCount) {
+    for (int i = 1; i < 8; i++) {
+        if (arr[i] == 1) {
+            totalCount += 1;
+        }
+    }
+    if (totalCount % 2 == 0) {
+        arr[9] = 0;
+    } else {
+
+        arr[9] = 1;
+    }
+}
+
+void receiver_calculate_parity(char arr[], char recarr[], int totalCount) {
+    totalCount = 0;
+    for (int i = 1; i < 8; i++) {
+        if (arr[i] == 1) {
+            totalCount += 1;
+        }
+    }
+    if (totalCount % 2 == 0) {
+        arr[9] = 0;
+    } else {
+
+        arr[9] = 1;
+    }
+
+    // if(){
+    //     printf("No Errors!");
+    // }else{
+    //     printf("Error: parity bit does not match!");
+    // }
+}
+
+void ec_encode(int * sendArr, int * retArr){
+
+   
+    retArr[4] = (sendArr[0] + sendArr[2] + sendArr[3]) %2;
+    retArr[5] = (sendArr[0] + sendArr[1] + sendArr[3]) %2;
+    retArr[6] = (sendArr[1] + sendArr[2] + sendArr[3]) %2;
+    retArr[7] = (sendArr[0] + sendArr[1] + sendArr[2] + sendArr[3] + retArr[8] + retArr[9] + retArr[10]) %2;
+
+    retArr[12] = (sendArr[4] + sendArr[6] + sendArr[7]) %2;
+    retArr[13] = (sendArr[4] + sendArr[5] + sendArr[7]) %2;
+    retArr[14] = (sendArr[5] + sendArr[6] + sendArr[7]) %2;
+    retArr[15] = (sendArr[4] + sendArr[5] + sendArr[6] + sendArr[7] + retArr[12] + retArr[13] + retArr[14]) %2;
+
+    for (int i = 0; i <4; i++){
+        retArr[i] = sendArr[i];
+    }
+    
+    for (int i = 0; i <4; i++){
+        retArr[i+8] = sendArr[i+4];
+    }
+    
+    // for (int i = 0; i < 16; i++)
+    // {
+    //     printf("%d", retArr[i]);
+    
+    // }   
+    // printf("\n");
+}
+
+void ec_decode(int * decArr, int * decRetArr){
+
+    int p1 = (decArr[0] + decArr[2] + decArr[3]) %2;
+    int p2 = (decArr[0] + decArr[1] + decArr[3]) %2;
+    int p3 = (decArr[1] + decArr[2] + decArr[3]) %2;
+    int p4 = (decArr[0] + decArr[1] + decArr[2] + decArr[3] + p1 + p2 +p3) %2;
+
+    int p5 = (decArr[8] + decArr[10] +decArr[11]) %2;
+    int p6 = (decArr[8] + decArr[9] + decArr[11]) %2;
+    int p7 = (decArr[9] + decArr[10] + decArr[11]) %2;
+    int p8 = (decArr[8] + decArr[9] + decArr[10] + decArr[11] + p5 + p6 +p7) %2;
+    
+    int c_0 = p1 == decArr[4]; //boolints: 1 true 0 false 
+    int c_1 = p2 == decArr[5];
+    int c_2 = p3 == decArr[6];
+    int c_3 = p4 == decArr[7];
+    int c_4 = p5 == decArr[12];
+    int c_5 = p6 == decArr[13];
+    int c_6 = p7 == decArr[14];
+    int c_7 = p8 == decArr[15];
+
+   // printf("1 bit flipped\n");
+    for (int i = 0; i < 16; i++)
+    {
+       // printf("%d", decArr[i]);
+    
+    }   
+//    printf("\n");
+    if(c_0&&c_1&&c_2&&c_3&&c_4&&c_5&&c_6&&c_7) {
+        // printf("Decoded with no errors!!!\n");
+
+    } 
+    else if (c_3 && (!c_1 || !c_2 || !c_0)){
+        printf("Two errors found.\n");
+    }
+    else if (c_7 && (!c_5 || !c_6 || !c_4)){
+        printf("Two errors found.\n");
+    }
+
+    else if(!c_0&&!c_1&&!c_2){
+        bitFlip(decArr, 3);
+    // }else if (c_3 && c_7){
+    //     printf("Two errors 3!\n");
+     } else if(!c_0&&!c_1&&!c_3){
+        //flip the bit
+        bitFlip(decArr, 0);
+    }else if(!c_1&&!c_2&&!c_3){
+        bitFlip(decArr, 1);
+    }else if(!c_0&&!c_2&&!c_3){
+        bitFlip(decArr, 2);
+    } 
+    else if(!c_0){
+        bitFlip(decArr, 4);
+    }else if(!c_1){
+        bitFlip(decArr, 5);
+    }else if(!c_2){
+        bitFlip(decArr, 6);
+    }else if(!c_3){
+        bitFlip(decArr, 7);
+    } 
+    /* SECOND HALF */
+    
+    if(!c_4&&!c_5&&!c_6){
+        bitFlip(decArr, 11);
+    }else if(!c_4&&!c_5&&!c_7){
+        //flip the bit
+        bitFlip(decArr, 8);
+    }else if(!c_5&&!c_6&&!c_7){
+        bitFlip(decArr, 9);
+    }else if(!c_4&&!c_6&&!c_7){
+        bitFlip(decArr, 10);
+    } 
+    
+    else if(!c_4){
+        bitFlip(decArr, 12);
+    }else if(!c_5){
+        bitFlip(decArr, 13);
+    }else if(!c_6){
+        bitFlip(decArr, 14);
+    }else if(!c_7){
+        bitFlip(decArr, 15);
+    
+    }
+    //recalculate
+    decRetArr[0] = decArr[0];
+    decRetArr[1] = decArr[1];
+    decRetArr[2] = decArr[2];
+    decRetArr[3] = decArr[3];
+
+    decRetArr[4] = decArr[8];
+    decRetArr[5] = decArr[9];
+    decRetArr[6] = decArr[10];
+    decRetArr[7] = decArr[11];
+
+   // printf("Bit flipped back\n");
+    for (int i = 0; i < 16; i++)
+    {
+        //printf("%d", decArr[i]);
+    
+    }   
+   // printf("\n");
+}
+
+void bitFlip(int * arr, int i){
+    if(arr[i] == 0){
+            arr[i] = 1;
+        }else{
+            arr[i] = 0;
+        }
+}
+
+void tester(){
+    // for(int i = 96; i <= 255; i++) {
+    //   fprintf(stdout, "[%d]: %c\n", i, i);
+    // }
+    int *decArr = malloc(sizeof(int)*16); 
+    //first bit ignored to set    0110110000101011
+    decArr[0] = 0;
+    decArr[1] = 1;
+    decArr[2] = 1;
+    decArr[3] = 0;
+    decArr[4] = 1;
+    decArr[5] = 1;
+    decArr[6] = 0;
+    decArr[7] = 0;
+    decArr[8] = 0;
+    decArr[9] = 0;
+    decArr[10] = 1;
+    decArr[11] = 0;
+    decArr[12] = 1;
+    decArr[13] = 0;
+    decArr[14] = 1;
+    decArr[15] = 1;
+    int *deRetArr = malloc(sizeof(int)*8); 
+
+
+
+    for (int i = 0; i < 16; i++){
+        printf("Idx: %d\n", i);
+        //int correct = decArr[i];
+        decArr[i] = !decArr[i];
+        ec_decode(decArr, deRetArr);
+
+    }
+
+
+}
--- piMsg.c
+++ piMsg.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include "checksum.c"
+#include "checksum.h"
 	//KNOWN PINS
 	//Port 1 tx: 27
 	//Port 1 rx: 26
@@ -22,7 +22,7 @@ uint32_t startTick = 0;
 uint32_t STARTTEST = 0;
 int count = 0;
 #define DELTA_T 6000
-#define MAX_BITS 1600 // maximum number of bits we can record in a message
+#define MAX_BITS 6400 // maximum number of bits we can record in a message
 char* bitBuffer;
 int bitCt = 0;
 int lastEdgeDir =-1; //no prev edge
@@ -41,6 +41,7 @@ int main(){
 	for(int i = 0; i <20; i++){
 		printf("Receiving:\n");
 		receiveHandshake(pi); // wait until something has been sent --> switch back
+
 		printf("Sending:\n");
 		send_mode(pi);
 	}	
@@ -61,15 +62,16 @@ int main(){
 int send_mode(int pi){
 
 		//read ASCII bits user sends (same size 101)
-		char text[200];
+		char text[400];
+		//char * text = calloc(201, sizeof(char));
 		char *binary;
 		int binaryLength;
     	printf("Enter a sentence (max 100 characters): ");
     	//	scanf("%100[^\n]", text); //read any char except newline char [^\n], max100
 		//scanf("%100s[^\n]", text);	
 		fgets(text, sizeof(text), stdin);
-		binaryLength = strlen(text) * 8; //8 bit ASCII chars
-		binary =  malloc(binaryLength + 1); //+1 for null ptr
+		binaryLength = strlen(text) * 16; //16 bit ASCII chars
+		binary =  calloc(binaryLength + 1, sizeof(char)); //+1 for null ptr
 
 		if (binary == NULL) {
 			fprintf(stderr, "Memory alloc failed\n");
@@ -120,13 +122,15 @@ int send_info(int pi, int bit){
 
 }
 char binaryToASCII(char *binary){
-//	printf("Binary to ascii running\n");
+	//printf("Binary to ascii running\n");
 	double decimal = 0.0;
 	// take our 16 bits, convert to 8 bits using decode
+ 	
 	int * return_arr = malloc(sizeof(int)*8);
-	int* param_arr = malloc(sizeof(int)*16);
+	int* param_arr = malloc(sizeof(int)*16); //empty array to be filled
 	for (int i = 0; i <16; i++){
 		param_arr[i] = (int) binary[i];
+		
 	}
 
 	ec_decode(param_arr, return_arr);
@@ -137,7 +141,7 @@ char binaryToASCII(char *binary){
 	//	printf("%d\n", binary[i]);
 		double base = 2.0;
 		double exp = 7-i;	
-		decimal += return_arr[i] * (pow(base,exp));
+		decimal += return_arr[i] * (pow(base,exp)); // TODO: switch to return_arr[i]
 		
 		//asciiChar <<= 1;
 		//if(binary[i] == '1'){
@@ -156,35 +160,70 @@ char binaryToASCII(char *binary){
 }
 //convert single character uinto its 8bit binary representation
 void charToBinary(unsigned char decimal, char *eight) { //char =ascii char, eight==char arr for binary string to be stored (9 long to hold 8 bit and terminator)
-    //bitwise to extract bits
+  //  printf("in char to bin\n");
+	
+	//bitwise to extract bits
 	for(int i = 7; i >= 0; --i) {
         eight[7 - i] = ((decimal >> i) & 1) + '0';
     }
-	int* encode_param = malloc(sizeof(int)*8);
-	int* encode_ret = malloc(sizeof(int)*16);
+	
+	int* encode_param = calloc(8, sizeof(int));
+	int* encode_ret = calloc(16, sizeof(int));
+
+
 	// convert eight to int
+//	printf("eight: \n");
 	for (int i= 0; i < 8; i++){
-		encode_param[i] = (int) eight[i];
+		int dec = (int) eight[i];
+		if (dec == 48){
+			encode_param[i] = 0;
+		}
+		else{
+			encode_param[i] = 1;
+		}
+//		printf("%c",eight[i]);
 	}
+//	printf("\ndone\n");
 	// call encode function
+//	printf("After switching to ints\n");
+//	for (int i = 0; i < 8; i++){
+//		printf("%d", encode_param[i]);
+//	}
+//	printf("\ndone\n");
 	ec_encode(encode_param, encode_ret);
 
 	for(int i = 0; i<16; i++){
-		char temp = (char) encode_ret;
-		eight[i] = temp;
+//		printf("%d", encode_ret[i]);
+	
+		if (encode_ret[i]  == 0){
+			eight[i] = '0';
+			
+			
+		}
+		else{
+			eight[i] =  '1';
+		}
+
+//		char temp = (char) encode_ret[i];
+//		eight[i] = temp;
+//		printf("%c", eight[i]);
+		
 	}
 	// switch the sixteen return arr to char 
-	
+		
 	free(encode_param);
 	free(encode_ret);
 
-    eight[8] = '\0'; // Null-terminate the string
+
+    	eight[16] = '\0'; // Null-terminate the string
 }
 //converts string of ASCII chars to binary string (goes through each char and concatennates results)
 void asciiToBinary(char *text, char *binary) { //exclude spaces (line25)
-    char *eight = malloc(17); //alloc 8-bit mem to represent each character sent
-    if (eight == NULL)
-        exit(1);
+//	printf("in ascii to bin\n");
+       	char *eight = calloc(17,sizeof(char)); //alloc 8-bit mem to represent each character sent
+    if (eight == NULL){
+	    exit(1);
+    }
     while(*text) // loop through each char
     {
         charToBinary(*text, eight); //convert char to binary
@@ -196,17 +235,23 @@ void asciiToBinary(char *text, char *binary) { //exclude spaces (line25)
         ++text; //move to next character in input txt
     }
     *binary = '\0'; //nullterm
+
     // binary -= binaryLength;
+    //printf("before free eight\n");
     free(eight);
+  //  printf("After free eight");
 }
 bool withinBuffer(uint32_t tick, int edgeDirection){
+	
+//	printf("%d", edgeDirection);
 	// set buffer of 5000
     int buff = 5000;
     uint32_t correctTick = startTick + (2 * DELTA_T);
-   //int diff = correctTick - tick;
-	//	printf("correctTick: %u, tick: %u, diff: %d\n", correctTick, tick, diff);
+   int diff = correctTick - tick;
+//	printf("correctTick: %u, tick: %u, diff: %d\n", correctTick, tick, diff);
 
     if((tick < (correctTick + buff)) && (tick > (correctTick - buff))){
+	
 //	printf("Within buffer\n");
        // printf("Within range: %d\n", edgeDirection);
 		//printf("Tick within range. \n");
@@ -217,15 +262,16 @@ bool withinBuffer(uint32_t tick, int edgeDirection){
             //store bits in bitBuffer
 	bitBuffer[bitCt++] = edgeDirection;
             // check to see if we have received 8 bits ( each char 8 bits)
-            if (bitCt % 16 == 0){
+            if (bitCt % 16 == 0){ //TODO change back to 16 instead of 8, as well as the next 4 lines
 		    //printf("Byte sent\n");
                 char byteStr[16];
-                for(int i = 0; i < 8; i++){
+                for(int i = 0; i < 16; i++){
                     byteStr[i] = bitBuffer[bitCt - 16+i];
+		    
                 }
                 byteStr[16] = '\0';
-		
                 char asciiChar = binaryToASCII(byteStr); //converts bites receeived to ASCII chars
+		
                 printf("%c", asciiChar);
                 fflush(stdout); 
             }
@@ -291,7 +337,7 @@ int receiveHandshake(int pi){
 	//	usleep(1000);
 		clock_t current = clock();
 		int diff = current - (int) lastFallTime;	
-		if ((lastFallTime != 0) && (diff > 20*DELTA_T)){
+		if ((lastFallTime != 0) && (diff > 40*DELTA_T)){
 			printf("\nreceiving over\n");
 			lastFallTime = 0;
 			hasRisen = false;
